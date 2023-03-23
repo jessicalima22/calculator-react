15b1b6e3a58831328ccd41d86f850d232689411d
jessicalima22 <jessica.lima22@hotmail.com>
2023-03-23 02:55:28 -0300

    need repare equal sign
    Css almost ok

--- Component/Operations.jsx
+++ Component/Operations.jsx
@@ -1,14 +1,30 @@
+import { useEffect } from 'react'
 import styles from '../styles/operations.module.css'
-export default function Operations (){
-    const operations = ["-", "/", "+", "x", " ", "%", "=", "="]
+
+const operations = ["-", "+", " ", "/", "x", "%", "="]
 
     const generateOperations = ()=>{
         return(
             <>
-            {operations.map(e=><div className={styles.operations}> {e} </div>)}
+            {operations.map((e,i)=><div key={i} id={i} className={styles.operations}> {e} </div>)}
            </>
         )
     }
+
+    
+
+
+export default function Operations (){
+    
+    useEffect(()=> {
+        const increase=document.getElementById("1")
+        increase.classList.add("doubleButtonIncrease")
+
+        const equal=document.getElementById("6")
+        equal.classList.add("doubleButtonEqual")
+
+    });
+        
 
     return (
         <>
@@ -16,3 +32,4 @@ export default function Operations (){
         </>
     )    
 }
+
